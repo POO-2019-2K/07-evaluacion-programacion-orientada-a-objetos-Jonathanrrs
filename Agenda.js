949438f569490219ae4f9a422f4b8d37d8048320
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2019-06-01 19:53:43 -0500

    local storage

--- Agenda.js
+++ Agenda.js
@@ -3,9 +3,19 @@ export default class Agenda {
     constructor(tablaAgenda) {
         this._tablaAgenda = tablaAgenda;
         this._tareas = [];
-       
+        this._initTables();
+        //localStorage.removeItem("tareas");
     }
-    
+    _initTables() {
+        let tareas = JSON.parse(localStorage.getItem("tareas"));
+        if (tareas === null) {
+            return;
+        }
+        tareas.forEach((tarea, index) => {
+            tarea.fecha = new Date(tarea.fecha);
+            this._showInTable(new Tarea(tarea));
+        });
+    }
     _showInTable(tarea) {
         let row = this._tablaAgenda.insertRow(-1);
         let cellNombre = row.insertCell(0);
@@ -16,14 +26,14 @@ export default class Agenda {
 
         let objTarea = {
             nombre: tarea.nombre,
-            fechaNac: tarea.fecha
+            fecha: tarea.fecha
         }
         this._tareas.push(objTarea);
     }
 
     addTarea(tarea) {
         this._showInTable(tarea);
-        
+        localStorage.setItem("tareas", JSON.stringify(this._tareas));
     }
    
 }
